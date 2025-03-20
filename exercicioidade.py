d4f97c15cc312e7a281a507afbce19fdf2552231
americogabriel <gabrieldouble007@gmail.com>
2025-03-20 17:04:47 -0300

    Add files via upload

---
+++ exercicioidade.py
@@ -0,0 +1,8 @@
+anonascimento = int(input('qual seu ano de nascimento?'))
+anoatual = 2025
+
+idadeatual = anoatual - anonascimento
+idade2030 = idadeatual + 5
+
+print ('Você possui,',idadeatual)
+print ('e em 2030, terá',idade2030)
